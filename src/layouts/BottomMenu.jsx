018d8621386993851a301265a8b040c540ac3571
unknown <OverflowDev>
2022-09-07 13:12:54 +0100

    pool and farm

--- src/layouts/BottomMenu.jsx
+++ src/layouts/BottomMenu.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 function BottomMenu() {
+  const active = 'font-bold'
+  const notActive = 'text-gray-600 hover:text-gray-900'
+
   return (
     <footer className='mt-auto md:hidden sticky bottom-0 bg-white'>
         <div className='flex justify-between items-center border-t py-6 px-6  gap-3'>
@@ -9,31 +12,59 @@ function BottomMenu() {
             <div className='flex justify-center'>
               <ion-icon name="swap-horizontal-outline" size='large'></ion-icon>
             </div>
-            <h2 className='text-center font-bold'>Swap</h2>
+            <NavLink 
+                to='/swap' 
+                className={
+                    ({isActive}) => isActive ? active : notActive 
+                }
+              >
+                Swap
+            </NavLink>
           </div>
 
-          <div className='flex flex-col text-md text-gray-500'>
+          <div className='flex flex-col text-md '>
             <div className='flex justify-center'>
               <ion-icon name="reorder-two-outline" size='large'></ion-icon>
             </div>
-            <h2 className='text-center'>Pool</h2>
+              <NavLink 
+                to='/pool' 
+                className={
+                    ({isActive}) => isActive ? active : notActive 
+                }
+              >
+                Pool
+              </NavLink>
           </div>
 
-          <div className='flex flex-col text-md text-gray-500'>
+          <div className='flex flex-col text-md '>
             <div className='flex justify-center'>
               <ion-icon name="alert-circle-outline" size='large'></ion-icon>
             </div>
-            <h2 className='text-center'>Farm</h2>
+            <NavLink 
+                to='/farm' 
+                className={
+                    ({isActive}) => isActive ? active : notActive 
+                }
+              >
+                Farm
+              </NavLink>
           </div>
           
-          <div className='flex flex-col text-md text-gray-500'>
+          <div className='flex flex-col text-md '>
             <div className='flex justify-center'>
               <ion-icon name="link-outline" size='large'></ion-icon>
             </div>
-            <h2 className='text-center'>Bridge</h2>
+            <NavLink 
+                to='/bridge' 
+                className={
+                    ({isActive}) => isActive ? active : notActive 
+                }
+              >
+                Bridge
+              </NavLink>
           </div>
 
-          <div className='flex flex-col text-md text-gray-500'>
+          <div className='flex flex-col text-md '>
             <div className='flex justify-center'>
               <ion-icon name="wallet-outline" size='large'></ion-icon>
             </div>
